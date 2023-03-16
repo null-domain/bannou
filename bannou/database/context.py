b5c84c5e41e12265ae6bb0362a8ada010c9df54a
nulldomain <git@nulldoma.in>
2023-03-16 23:41:50 +0000

    Update bannou/database/context.py
    Co-authored-by: davfsa <davfsa@gmail.com>

--- bannou/database/context.py
+++ bannou/database/context.py
@@ -4,10 +4,6 @@ from ormar import ModelMeta
 
 from bannou import bot_settings
 
-_database = Database(bot_settings.postgres.build_url())
-_metadata = sqlalchemy.MetaData()
-
-
 class BaseMeta(ModelMeta):
-    database = _database
-    metadata = _metadata
+    database = Database(bot_settings.postgres.build_url())
+    metadata = sqlalchemy.MetaData()
