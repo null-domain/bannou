ad8668572609e75050efcce093e167f5ed9fb2af
nulldomain <git@nulldoma.in>
2023-03-28 18:00:31 +0100

    Change alembic to use new base class properties
    Since we're not using the databases module anymore, alembic needs to pull the database url directly from settings

--- scripts/migrations/env.py
+++ scripts/migrations/env.py
@@ -11,8 +11,8 @@ from sqlalchemy import engine as sqlalchemy_engine
 from sqlalchemy import pool as sqlalchemy_pool
 from sqlalchemy.ext import asyncio as sqlalchemy_asyncio
 
-from bannou import database as bannou_database
-from bannou.database import base as bannou_models_base
+from bannou import settings as bannou_settings
+from bannou.database import base as bannou_database
 
 # Detect and import module objects
 models_modules_path = pathlib.Path(bannou_database.__file__).parent
@@ -25,7 +25,7 @@ for models_module_path in models_modules_path.iterdir():
 
 # Define variables to use bellow
 config = alembic_context.config
-target_metadata = bannou_models_base.BaseMeta.metadata
+target_metadata = bannou_database.BaseMeta.metadata
 
 # Init logging
 if config.config_file_name:
@@ -33,7 +33,7 @@ if config.config_file_name:
 
 
 # Set defaults
-config.set_main_option("sqlalchemy.url", str(bannou_models_base.BaseMeta.database.url))
+config.set_main_option("sqlalchemy.url", bannou_settings.BOT_SETTINGS.postgres.build_url())
 
 
 def run_migrations_offline() -> None:
