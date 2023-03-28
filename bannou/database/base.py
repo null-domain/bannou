13df7ebd8888e8888a68939247c221e62ed4c7d7
nulldomain <git@nulldoma.in>
2023-03-28 17:59:40 +0100

    Recreate model base meta class

--- bannou/database/base.py
+++ bannou/database/base.py
@@ -1,15 +1,7 @@
 from __future__ import annotations
 
-import sqlalchemy
-from databases import Database
-from ormar import ModelMeta
-
-from bannou import settings
-
-DATABASE = Database(settings.BOT_SETTINGS.postgres.build_url())
-METADATA = sqlalchemy.MetaData()
+import sqlalchemy.orm
 
 
-class BaseMeta(ModelMeta):
-    database = DATABASE
-    metadata = METADATA
+class BaseMeta(sqlalchemy.orm.DeclarativeBase):
+    pass
