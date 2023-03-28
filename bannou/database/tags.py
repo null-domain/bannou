3d4bc9fd2fbfdc68fb8f4d5909b50f9422fceb93
nulldomain <git@nulldoma.in>
2023-03-28 18:01:45 +0100

    Rewrite tags table as an SQLA model

--- bannou/database/tags.py
+++ bannou/database/tags.py
@@ -2,24 +2,24 @@ from __future__ import annotations
 
 __all__: typing.Sequence[str] = ("Tag",)
 
+import datetime
 import typing
 
-import ormar
 import sqlalchemy
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
 
 from bannou.database import base
 
-if typing.TYPE_CHECKING:
-    import datetime
 
+class Tag(base.BaseMeta):
+    __tablename__ = "tags"
 
-class Tag(ormar.Model):
-    class Meta(base.BaseMeta):
-        tablename = "tags"
-
-    id: int = ormar.Integer(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
     # TODO: created_by column mapping to an individual user via User model
-    created_on: datetime.datetime = ormar.DateTime(nullable=False, server_default=sqlalchemy.func.now())
-    tag_name: str = ormar.String(max_length=32, nullable=False)
-    uses: int = ormar.Integer(default=0, nullable=False)
-    content: str = ormar.String(max_length=4000, nullable=False)
+    tag_name: Mapped[str] = mapped_column(sqlalchemy.String(length=32), nullable=False)
+    content: Mapped[str] = mapped_column(sqlalchemy.Text(), nullable=False)
+    created_on: Mapped[datetime.datetime] = mapped_column(
+        sqlalchemy.DateTime(timezone=True), nullable=False, server_default=sqlalchemy.func.now()
+    )
+    uses: Mapped[int] = mapped_column(sqlalchemy.Integer(), nullable=False, server_default=sqlalchemy.text("'0'"))
