002341e0e028a031d8f669ce239512e11214f745
nulldomain <git@nulldoma.in>
2023-03-30 23:43:24 +0100

    Make a type alias for SQLA async session maker

--- bannou/database/base.py
+++ bannou/database/base.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
+import sqlalchemy.ext.asyncio
 import sqlalchemy.orm
+
+AsyncSession = sqlalchemy.ext.asyncio.async_sessionmaker[sqlalchemy.ext.asyncio.AsyncSession]
 
 
 class BaseMeta(sqlalchemy.orm.DeclarativeBase):
