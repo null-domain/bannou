fb822aa0a64b59e80625999c9df2c6dae782108d
nulldomain <git@nulldoma.in>
2023-04-02 16:01:52 +0100

    Add SQL schema for database

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -1,6 +1,7 @@
-CREATE TABLE IF NOT EXISTS version (
+CREATE TABLE IF NOT EXISTS schema_version (
     schema_ver INTEGER NOT NULL,
     inserted_at TIMESTAMPTZ,
+    PRIMARY KEY(schema_ver)
 );
 
 CREATE TABLE IF NOT EXISTS guilds (
