8201518b43b503d334455658c472fdc448afae37
nulldomain <git@nulldoma.in>
2023-04-06 00:33:58 +0100

    Import classes explicitly to prevent flake8 F403, export using __all__ to stop mypy crying about not explicitly (re)exporting classes

--- bannou/database/__init__.py
+++ bannou/database/__init__.py
@@ -1,5 +1,9 @@
 from __future__ import annotations
 
+import typing
+
 from bannou.database import base
-from bannou.database.guilds import *
-from bannou.database.tags import *
+from bannou.database.guilds import Guild
+from bannou.database.tags import Tag
+
+__all__: typing.Sequence[str] = ("Guild", "Tag")
