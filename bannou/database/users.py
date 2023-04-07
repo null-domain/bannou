89813b85422ae5d32bdc646682b4e8b22a6833dc
nulldomain <git@nulldoma.in>
2023-04-07 21:57:58 +0100

    Remove guild mappings from User table
    This does not actually work

--- bannou/database/users.py
+++ bannou/database/users.py
@@ -15,8 +15,5 @@ if typing.TYPE_CHECKING:
 
 class User(base.BaseMeta):
     __tablename__: str = "users"
-    __table_args__: typing.Any = (sqlalchemy.PrimaryKeyConstraint("id", "guild_id"),)
 
-    id: orm.Mapped[int] = orm.mapped_column(sqlalchemy.BigInteger())
-    guild_id: orm.Mapped[int] = orm.mapped_column(sqlalchemy.ForeignKey("guilds.id"))
-    guild: orm.Mapped[Guild] = orm.relationship(back_populates="users")
+    id: orm.Mapped[int] = orm.mapped_column(sqlalchemy.BigInteger(), primary_key=True, autoincrement=False)
